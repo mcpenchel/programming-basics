34ac0fb0c16b4178cca9a4baa58f091ebe22116c
Matheus Penchel <matheus.penchel@gmail.com>
2020-10-14 17:28:44 -0300

    First version working, using str concatenation

---
+++ livecodes/acronymize/acronymize.rb
@@ -0,0 +1,30 @@
+# Rule nr 7 about methods
+# -> Methods should NEVER puts
+# -> Methods should NEVER gets.chomp
+
+def acronomyzer(sentence)
+  words = sentence.split
+  letters = ""
+
+  words.each do |word|
+    letter = word[0]
+    letters += letter
+  end
+
+  letters.upcase
+end
+
+# Acronymizer method (of sorts)
+# puts something == something_else
+
+puts acronomyzer("Mastodon is the best band").class == String
+puts acronomyzer("What the fuck") == "WTF"
+puts acronomyzer("") == ""
+
+
+puts acronomyzer("not safe for work")
+puts acronomyzer("missing in action")
+puts acronomyzer("oh my god")
+puts acronomyzer("laughing out loud")
+
+
