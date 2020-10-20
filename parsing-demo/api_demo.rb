30832d38408a7ffe8aeaa5eba790dc20a3e3c5b4
Matheus Penchel <matheus.penchel@gmail.com>
2020-10-20 07:54:55 -0300

    Added more API's to showcase at class

--- parsing-demo/api_demo.rb
+++ parsing-demo/api_demo.rb
@@ -3,3 +3,5 @@ require 'open-uri'
 
 # TODO - Let's fetch name and bio from a given GitHub username
 url = 'https://api.github.com/users/ssaunier'
+url = 'https://api.quotable.io/random'
+url = 'https://api.chucknorris.io/jokes/random'
