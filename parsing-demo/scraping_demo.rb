0c860f94eb4ba3d564d529517e4e02c6540e2295
Matheus Penchel <matheus.penchel@gmail.com>
2020-10-20 08:05:56 -0300

    Added more URL's to parse for displaying in class

--- parsing-demo/scraping_demo.rb
+++ parsing-demo/scraping_demo.rb
@@ -2,3 +2,6 @@ require 'open-uri'
 require 'nokogiri'
 
 # Let's scrape recipes from http://www.epicurious.com
+url = 'http://www.epicurious.com'
+url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+url = 'https://www.bbc.com/news'
