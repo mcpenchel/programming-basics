b204ee4d4dc0188d8918dfb060041f4a22175e8b
Matheus Penchel <matheus.penchel@gmail.com>
2020-11-11 11:38:16 -0300

    Improved the comments

--- http-and-ajax/src/index.js
+++ http-and-ajax/src/index.js
@@ -3,6 +3,10 @@ const ul = document.getElementById('results');
 const form = document.getElementById('search-movies');
 
 const fetchAddresses = (keyword) => {
+  // By default, fetch makes a GET request.
+  // So to do a POST request with AJAX, we need to specify
+  // a Javascript Object as a second argument to the method,
+  // like following:
   fetch('https://places-dsn.algolia.net/1/places/query', {
     method: 'POST',
     body: JSON.stringify({query: keyword})
@@ -18,6 +22,13 @@ const fetchAddresses = (keyword) => {
         ul.insertAdjacentHTML('beforeend', li);
       });
     })
+
+  // You will always need the JSON.stringify({}) when sending
+  // information on the body of the request.
+
+  // You will always need the two "then", be it a POST, GET,
+  // PUT, PATCH or DELETE request :ok They are for treating the
+  // information received from the API.
 }
 
 const fetchMovies = (keyword) => {
@@ -26,6 +37,9 @@ const fetchMovies = (keyword) => {
   fetch(`http://www.omdbapi.com/?apikey=adf1f2d7&s=${keyword}`)
     .then(response => response.json())
     .then((data) => {
+      // To delete everything inside an ul, or div, or any
+      // type of HTML element, just assign an empty string to
+      // it's innerHTML property ;)
       ul.innerHTML = '';
 
       data.Search.forEach((movieObject) => {
@@ -43,7 +57,8 @@ const fetchMovies = (keyword) => {
 // OMDB API gives us!
 form.addEventListener('submit', (event) => {
   event.preventDefault(); // we always need this when doing
-  // ajax or javascript on a submission of a form
+  // ajax or javascript on a submission of a form, or else
+  // the form will be submitted and the page will reload ;__;
   fetchMovies(textInput.value);
 })
 
@@ -54,7 +69,7 @@ textInput.addEventListener('keyup', (event) => {
   fetchAddresses(textInput.value);
 })
 
-
-// What are the elements involved? => querySelecting your elements
-// When does something happen? => addEventListener
-// What happens? => coding the callback of the eventListener
+// JavaScript mindset: the three fundamental questions!
+// 1) What are the elements involved? => querySelecting your elements
+// 2) When does something happen? => addEventListener
+// 3) What happens? => coding the callback of the eventListener
