d4ea007941b0bfb5b29b5a768a8d8936751a446f
Matheus Penchel <matheus.penchel@gmail.com>
2020-11-11 08:32:35 -0300

    Lecture preparation for HTTP and AJAX

---
+++ http-and-ajax/src/index.js
@@ -0,0 +1,12 @@
+// OMDB API: http://www.omdbapi.com/
+// API key: adf1f2d7
+
+// 1) Fetch movies from the OMDB API with an AJAX request
+
+// 2) Create an li for each movie retrieved from the API
+
+// 3) Instead of going to the API whenever the page loads,
+// let's connect it to the search form :D
+
+// 4) Reverse engineer the POST request of Algolia Places
+// https://community.algolia.com/places/
